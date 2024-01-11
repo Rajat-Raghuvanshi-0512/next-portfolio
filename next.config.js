9a009ed2f66eb72cb267c23584146e0479ba3b01
Rajat <rajat.karnal@gmail.com>
2024-01-12 01:17:13 +0530

    new build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/github-pages',
+  basePath: 'https://rajat-raghuvanshi-0512.github.io/next-portfolio',
 };
 
 module.exports = nextConfig;
